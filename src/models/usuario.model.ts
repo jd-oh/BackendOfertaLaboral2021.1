8c79f2412bc0bdbfe98900f3d0f7dedc70a13b41
Juan David Osorio <juan.1701714291@ucaldas.edu.co>
2021-03-11 08:08:01 -0500

    Creación de modelos y datasources

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  _id?: number;
+
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre_usuario: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
